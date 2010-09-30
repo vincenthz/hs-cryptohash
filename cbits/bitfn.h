0b949923b9b58467b283c332c7d901da55106ac2
Vincent Hanquez <vincent@snarc.org>
2010-09-30 07:48:59 +0100

    Fix support for macosx and add support for freebsd.
    macosx defined a __APPLE__ not a MACOSX as previously found.

--- cbits/bitfn.h
+++ cbits/bitfn.h
@@ -161,7 +161,9 @@ static inline void array_copy64(uint64_t *d, uint64_t *s, uint32_t nb)
 #ifdef __MINGW32__
   # define LITTLE_ENDIAN 1234
   # define BYTE_ORDER    LITTLE_ENDIAN
-#elif defined(MACOSX)
+#elif defined(__FreeBSD__)
+  # include <sys/endian.h>
+#elif defined(__APPLE__)
   # include <machine/endian.h>
 #else
   # include <endian.h>
