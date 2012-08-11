74d142c9ecf5bc96ea282552e21a73768cb69363
Stijn van Drongelen <self@tinctorius.net>
2012-08-11 12:26:37 +0200

    Whirlpool: use stdint.h instead of half-baked definitions

--- cbits/whirlpool_nessie.h
+++ cbits/whirlpool_nessie.h
@@ -2,45 +2,18 @@
 #ifndef PORTABLE_C__
 #define PORTABLE_C__
 
-#include <limits.h>
-
-/* Definition of minimum-width integer types
- * 
- * u8   -> unsigned integer type, at least 8 bits, equivalent to unsigned char
- * u16  -> unsigned integer type, at least 16 bits
- * u32  -> unsigned integer type, at least 32 bits
- *
- * s8, s16, s32  -> signed counterparts of u8, u16, u32
- */
-
-typedef signed char s8;
-typedef unsigned char u8;
-
-#if UINT_MAX >= 4294967295UL
-
-typedef signed short s16;
-typedef signed int s32;
-typedef unsigned short u16;
-typedef unsigned int u32;
-
-#else
-
-typedef signed int s16;
-typedef signed long s32;
-typedef unsigned int u16;
-typedef unsigned long u32;
-
-#endif
-
 #ifdef _MSC_VER
-typedef unsigned __int64 u64;
-typedef signed __int64 s64;
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
 #define LL(v)   (v##i64)
-#else  /* !_MSC_VER */
-typedef unsigned long long u64;
-typedef signed long long s64;
+#else
+#include <stdint.h>
 #define LL(v)   (v##ULL)
-#endif /* ?_MSC_VER */
+#endif
 
 /*
  * Whirlpool-specific definitions.
@@ -56,11 +29,11 @@ typedef signed long long s64;
 #define LENGTHBITS  (8*LENGTHBYTES) /* 256 */
 
 typedef struct NESSIEstruct {
-	u8  bitLength[LENGTHBYTES]; /* global number of hashed bits (256-bit counter) */
-	u8  buffer[WBLOCKBYTES];	/* buffer of data to hash */
+	uint8_t  bitLength[LENGTHBYTES]; /* global number of hashed bits (256-bit counter) */
+	uint8_t  buffer[WBLOCKBYTES];	/* buffer of data to hash */
 	int bufferBits;		        /* current number of bits on the buffer */
 	int bufferPos;		        /* current (possibly incomplete) byte slot on the buffer */
-	u64 hash[DIGESTBYTES/8];    /* the hashing state */
+	uint64_t hash[DIGESTBYTES/8];    /* the hashing state */
 } NESSIEstruct;
 
 #endif   /* PORTABLE_C__ */
