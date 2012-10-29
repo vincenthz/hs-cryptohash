9a5712df40d8f03029230aa019e29b1cfd013335
Vincent Hanquez <vincent@snarc.org>
2012-10-29 08:11:35 +0000

    remove unnecessary compability layer. everything should support stdint and ULL constants.

--- cbits/whirlpool.h
+++ cbits/whirlpool.h
@@ -1,18 +1,7 @@
 #ifndef CRYPTOHASH_WHIRLPOOL_H
 #define CRYPTOHASH_WHIRLPOOL_H
 
-#ifdef _MSC_VER
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef __int32 int32_t;
-typedef unsigned __int32 uint32_t;
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
-#define LL(v)   (v##i64)
-#else
 #include <stdint.h>
-#define LL(v)   (v##ULL)
-#endif
 
 /*
  * Whirlpool-specific definitions.
