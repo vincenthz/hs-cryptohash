7bb02f2548af5fe9ffd4d2beb8eaf61502343724
Vincent Hanquez <vincent@snarc.org>
2012-12-07 17:12:10 +0000

    add Context and Digest (w/ phantom types) and HashAlgorithm class.
    the phantom types is supposed to represent an hash algorithm class.

---
+++ Crypto/Hash/Types.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ExistentialQuantification #-}
+module Crypto.Hash.Types
+    ( HashAlgorithm(..)
+    , Context(..)
+    , Digest(..)
+    )
+    where
+
+import Data.ByteString (ByteString)
+
+class HashAlgorithm a where
+    hashInit     :: Context a
+    hashUpdates  :: Context a -> [ByteString] -> Context a
+    hashFinalize :: Context a -> Digest a
+
+newtype Context a = Context { contextToByteString :: ByteString }
+
+newtype Digest a = Digest { digestToByteString :: ByteString }
