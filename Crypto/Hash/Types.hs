5c2fdc1830ddfc80ff52da283f82b5e94bb3eee0
Vincent Hanquez <vincent@snarc.org>
2012-12-08 10:58:41 +0000

    add some docs.

--- Crypto/Hash/Types.hs
+++ Crypto/Hash/Types.hs
@@ -8,12 +8,28 @@ module Crypto.Hash.Types
 
 import Data.ByteString (ByteString)
 
+-- | Class representing hashing algorithms.
+--
+-- The hash algorithm is built over 3 primitives:
+--
+--   init     : create a new context
+--   updates  : update the context with some strict bytestrings
+--   finalize : finalize the context into a digest
+--
 class HashAlgorithm a where
+    -- | Initialize a new context for this hash algorithm
     hashInit     :: Context a
+
+    -- | Update the context with a list of strict bytestring,
+    -- and return a new context with the updates.
     hashUpdates  :: Context a -> [ByteString] -> Context a
+
+    -- | Finalize a context and return a digest.
     hashFinalize :: Context a -> Digest a
 
+-- | Represent a context for a given hash algorithm.
 newtype Context a = Context { contextToByteString :: ByteString }
 
+-- | Represent a digest for a given hash algorithm.
 newtype Digest a = Digest { digestToByteString :: ByteString }
     deriving (Eq,Ord)
