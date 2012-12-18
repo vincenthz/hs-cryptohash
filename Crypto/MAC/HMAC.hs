98945aaa72f622de9e3dad7280f437bc3738996e
Vincent Hanquez <vincent@snarc.org>
2012-12-18 07:06:15 +0000

    redefine hmac in term of Digest a

--- Crypto/MAC/HMAC.hs
+++ Crypto/MAC/HMAC.hs
@@ -15,13 +15,14 @@ module Crypto.MAC.HMAC
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Data.Bits (xor)
+import Crypto.Hash
 
 -- | compute a MAC using the supplied hashing function
-hmac :: (ByteString -> ByteString) -> Int -> ByteString -> ByteString -> ByteString
-hmac f blockSize secret msg = f $ B.append opad (f $ B.append ipad msg) where
-    opad = B.map (xor 0x5c) k'
-    ipad = B.map (xor 0x36) k'
+hmac :: (ByteString -> Digest a) -> Int -> ByteString -> ByteString -> ByteString
+hmac hashF blockSize secret msg = digestToByteString $ hashF $ B.append opad (digestToByteString $ hashF $ B.append ipad msg)
+    where opad = B.map (xor 0x5c) k'
+          ipad = B.map (xor 0x36) k'
 
-    k' = B.append kt pad
-    kt = if B.length secret > fromIntegral blockSize then f secret else secret
-    pad = B.replicate (fromIntegral blockSize - B.length kt) 0
+          k'  = B.append kt pad
+          kt  = if B.length secret > fromIntegral blockSize then digestToByteString (hashF secret) else secret
+          pad = B.replicate (fromIntegral blockSize - B.length kt) 0
