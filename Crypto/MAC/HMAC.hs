27a35f5851ce867c36fbc6f24b6ff5ea0c3ae762
Vincent Hanquez <vincent@snarc.org>
2012-12-27 07:13:40 +0000

    leave Crypto.MAC.HMAC as a simple non phantom types variant.

--- Crypto/MAC/HMAC.hs
+++ Crypto/MAC/HMAC.hs
@@ -15,14 +15,13 @@ module Crypto.MAC.HMAC
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Data.Bits (xor)
-import Crypto.Hash
 
 -- | compute a MAC using the supplied hashing function
-hmac :: (ByteString -> Digest a) -> Int -> ByteString -> ByteString -> ByteString
-hmac hashF blockSize secret msg = digestToByteString $ hashF $ B.append opad (digestToByteString $ hashF $ B.append ipad msg)
+hmac :: (ByteString -> ByteString) -> Int -> ByteString -> ByteString -> ByteString
+hmac hashF blockSize secret msg = hashF $ B.append opad (hashF $ B.append ipad msg)
     where opad = B.map (xor 0x5c) k'
           ipad = B.map (xor 0x36) k'
 
           k'  = B.append kt pad
-          kt  = if B.length secret > fromIntegral blockSize then digestToByteString (hashF secret) else secret
+          kt  = if B.length secret > fromIntegral blockSize then hashF secret else secret
           pad = B.replicate (fromIntegral blockSize - B.length kt) 0
