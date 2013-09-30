048b4e1b13b41248d1117fe273ce3273b93bad00
Vincent Hanquez <vincent@snarc.org>
2013-09-30 03:05:46 +0100

    add haddock comment for each hash exported

--- Crypto/Hash.hs
+++ Crypto/Hash.hs
@@ -117,30 +117,51 @@ instance HashAlgorithm NAME where \
     ; digestFromByteString bs = if B.length bs == len then (Just $ Digest bs) else Nothing where { len = B.length (MODULENAME.finalize (MODULENAME.init LEN)) } \
     };
 
+-- | MD2 cryptographic hash
 DEFINE_INSTANCE(MD2, MD2, 16)
+-- | MD4 cryptographic hash
 DEFINE_INSTANCE(MD4, MD4, 64)
+-- | MD5 cryptographic hash
 DEFINE_INSTANCE(MD5, MD5, 64)
+-- | SHA1 cryptographic hash
 DEFINE_INSTANCE(SHA1, SHA1, 64)
+-- | SHA224 cryptographic hash
 DEFINE_INSTANCE(SHA224, SHA224, 64)
+-- | SHA256 cryptographic hash
 DEFINE_INSTANCE(SHA256, SHA256, 64)
+-- | SHA384 cryptographic hash
 DEFINE_INSTANCE(SHA384, SHA384, 128)
+-- | SHA512 cryptographic hash
 DEFINE_INSTANCE(SHA512, SHA512, 128)
 
+-- | RIPEMD160 cryptographic hash
 DEFINE_INSTANCE(RIPEMD160, RIPEMD160, 64)
+-- | Whirlpool cryptographic hash
 DEFINE_INSTANCE(Whirlpool, Whirlpool, 64)
+-- | Tiger cryptographic hash
 DEFINE_INSTANCE(Tiger, Tiger, 64)
 
+-- | SHA3 (224 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(SHA3_224, SHA3, 224, 144)
+-- | SHA3 (256 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(SHA3_256, SHA3, 256, 136)
+-- | SHA3 (384 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(SHA3_384, SHA3, 384, 104)
+-- | SHA3 (512 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(SHA3_512, SHA3, 512, 72)
 
+-- | Skein256 (224 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(Skein256_224, Skein256, 224, 32)
+-- | Skein256 (256 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(Skein256_256, Skein256, 256, 32)
 
+-- | Skein512 (224 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(Skein512_224, Skein512, 224, 64)
+-- | Skein512 (256 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(Skein512_256, Skein512, 256, 64)
+-- | Skein512 (384 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(Skein512_384, Skein512, 384, 64)
+-- | Skein512 (512 bits version) cryptographic hash
 DEFINE_INSTANCE_LEN(Skein512_512, Skein512, 512, 64)
 
 -- | Initialize a new context for a specified hash algorithm
